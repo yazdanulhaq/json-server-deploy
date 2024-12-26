f2cf857f2a6c0fed70011f18d7317f13b0071066
yazdanul <yazdan.haq@camp1.tkxel.com>
2024-12-26 15:21:17 +0500

    update

---
+++ functions/api.js
@@ -0,0 +1,18 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('./data.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+module.exports.handler = async (event, context) => {
+  return new Promise((resolve, reject) => {
+    server.listen(3000, () => {
+      resolve({
+        statusCode: 200,
+        body: JSON.stringify({ message: "JSON Server is running" }),
+      });
+    });
+  });
+};
